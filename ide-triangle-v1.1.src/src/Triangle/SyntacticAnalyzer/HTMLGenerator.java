fbdf33a512de083bcd926b8887cabc5646f01da5
Esteban Madrigal <ememadrigal.em@gmail.com>
2021-04-18 22:19:01 -0600

    Update HTMLGenerator.java

--- ide-triangle-v1.1.src/src/Triangle/SyntacticAnalyzer/HTMLGenerator.java
+++ ide-triangle-v1.1.src/src/Triangle/SyntacticAnalyzer/HTMLGenerator.java
@@ -27,11 +27,8 @@ public final class HTMLGenerator {
 
   private char currentChar;
   private StringBuffer currentSpelling;
-  private boolean currentlyScanningToken;
-    private int cont;
-  public StringBuffer getCP(){
-      return this.currentSpelling;
-  }
+  private int cont;
+ 
   private boolean isLetter(char c) {
     return (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z');
   }
@@ -71,7 +68,7 @@ public final class HTMLGenerator {
                          "</style>" +
                          "<body>");
         this.generateHTML();
-        //HTML footer
+
         fileWriter.write("</body>\n" +
                          "</html>");
 
@@ -85,175 +82,159 @@ public final class HTMLGenerator {
 
 
   private void takeIt() {
-    if (currentlyScanningToken)
-      currentSpelling.append(currentChar);
+    currentSpelling.append(currentChar);
     currentChar = sourceFile.getSource();
   }
 
   private void scanToken() {
 
     switch (currentChar) {
-
-    case 'a':  case 'b':  case 'c':  case 'd':  case 'e':
-    case 'f':  case 'g':  case 'h':  case 'i':  case 'j':
-    case 'k':  case 'l':  case 'm':  case 'n':  case 'o':
-    case 'p':  case 'q':  case 'r':  case 's':  case 't':
-    case 'u':  case 'v':  case 'w':  case 'x':  case 'y':
-    case 'z':
-    case 'A':  case 'B':  case 'C':  case 'D':  case 'E':
-    case 'F':  case 'G':  case 'H':  case 'I':  case 'J':
-    case 'K':  case 'L':  case 'M':  case 'N':  case 'O':
-    case 'P':  case 'Q':  case 'R':  case 'S':  case 'T':
-    case 'U':  case 'V':  case 'W':  case 'X':  case 'Y':
-    case 'Z':
-      takeIt();
-      while (isLetter(currentChar) || isDigit(currentChar))
-        takeIt();
-      if (this.isReserved(this.currentSpelling.toString())){
-          writeBoldBlack(this.currentSpelling.toString());
-      }else{
-          writeSimpleBlack(this.currentSpelling.toString());
-      }
-      break;
-    case '0':  case '1':  case '2':  case '3':  case '4':
-    case '5':  case '6':  case '7':  case '8':  case '9':
-      takeIt();
-      while (isDigit(currentChar))
-        takeIt();
-        writeSimpleBlue(this.currentSpelling.toString());
-        break;
-    case '+':  case '-':  case '*': case '/':  case '=':
-    case '<':  case '>':  case '\\':  case '&':  case '@':
-    case '%':  case '^':  case '?':
-      takeIt();
-      while (isOperator(currentChar))
-        takeIt();
+        case 'a':  case 'b':  case 'c':  case 'd':  case 'e':
+        case 'f':  case 'g':  case 'h':  case 'i':  case 'j':
+        case 'k':  case 'l':  case 'm':  case 'n':  case 'o':
+        case 'p':  case 'q':  case 'r':  case 's':  case 't':
+        case 'u':  case 'v':  case 'w':  case 'x':  case 'y':
+        case 'z':
+        case 'A':  case 'B':  case 'C':  case 'D':  case 'E':
+        case 'F':  case 'G':  case 'H':  case 'I':  case 'J':
+        case 'K':  case 'L':  case 'M':  case 'N':  case 'O':
+        case 'P':  case 'Q':  case 'R':  case 'S':  case 'T':
+        case 'U':  case 'V':  case 'W':  case 'X':  case 'Y':
+        case 'Z':
+            takeIt();
+            while (isLetter(currentChar) || isDigit(currentChar))
+                takeIt();
+            if (this.isReserved(this.currentSpelling.toString())){
+                writeBoldBlack(this.currentSpelling.toString());
+            }else{
+                writeSimpleBlack(this.currentSpelling.toString());
+            }
+            break;
+        case '0':  case '1':  case '2':  case '3':  case '4':
+        case '5':  case '6':  case '7':  case '8':  case '9':
+            takeIt();
+            while (isDigit(currentChar))
+                takeIt();
+            writeSimpleBlue(this.currentSpelling.toString());
+            break;
+        case '+':  case '-':  case '*': case '/':  case '=':
+        case '<':  case '>':  case '\\':  case '&':  case '@':
+        case '%':  case '^':  case '?':
+            takeIt();
+            while (isOperator(currentChar))
+                takeIt();
             writeSimpleBlack(this.currentSpelling.toString());
-    break;
-    case '\'':
-      takeIt();
-      takeIt(); // the quoted character
-      if (currentChar == '\'') {
-      	takeIt();
-        writeSimpleBlue(this.currentSpelling.toString());
-      } else
-        System.out.println("Error");
-    break;
-    // agrega caso para RANGE y DOT
-    case '.':
-      takeIt();
-      if(currentChar == '.'){
-          takeIt();
+            break;
+        case '\'':
+            takeIt();
+            takeIt(); // the quoted character
+            if (currentChar == '\'') {
+                takeIt();
+            writeSimpleBlue(this.currentSpelling.toString());
+            }else
+                System.out.println("Error");
+            break;
+        case '.':
+            takeIt();
+            if(currentChar == '.'){
+                takeIt();
                 writeSimpleBlack(this.currentSpelling.toString());
-      }else{
+            }else{
                 writeSimpleBlack(this.currentSpelling.toString());
-      }
-    break;
-    case ':':
-      takeIt();
-      if (currentChar == '=') {
-        takeIt();
-        writeSimpleBlack(this.currentSpelling.toString());
-      } else{
-          writeSimpleBlack(this.currentSpelling.toString());
-      }
-    break;
-    case ';':
-      takeIt();
-      writeSimpleBlack(this.currentSpelling.toString());
-    break;
-    case ',':
-      takeIt();
-      writeSimpleBlack(this.currentSpelling.toString());
-    break;
-    case '~':
-      takeIt();
-      writeSimpleBlack(this.currentSpelling.toString());
-    break;      
-    // agrega case PIPE, DOLLAR
-    case '|':
-        takeIt();
-        writeSimpleBlack(this.currentSpelling.toString());
-    break;        
-    case '$':
-        takeIt();
-        writeSimpleBlack(this.currentSpelling.toString());
-    break;
-    case '(':
-        takeIt();
-        writeSimpleBlack(this.currentSpelling.toString());
-    break;
-    case ')':
-      takeIt();
-      writeSimpleBlack(this.currentSpelling.toString());
-    break;
-    case '[':
-      takeIt();
-      writeSimpleBlack(this.currentSpelling.toString());
-    break;
-    case ']':
-      takeIt();
-      writeSimpleBlack(this.currentSpelling.toString());
-    break;
-    case '{':
-      takeIt();
-      writeSimpleBlack(this.currentSpelling.toString());
-    break;
-
-    case '}':
-      takeIt();
-      writeSimpleBlack(this.currentSpelling.toString());
-    break;
-    case SourceFile.EOT:
-      this.isDone = true;
-    break;    
-    case '!':
-      {
-        takeIt();
-        while ((currentChar != SourceFile.EOL) && (currentChar != SourceFile.EOT))
-          takeIt();
-        writeSimpleGreen(this.currentSpelling.toString());
-      }
-    break;  
-    case ' ':
-        takeIt();
-        writeSimpleBlack(this.currentSpelling.toString());
-    break;
-    case '\n':
-    case '\r': 
-        takeIt();
-        try {
-            fileWriter.write("<br>");
-        } catch (IOException e) {
-            System.err.println("Error while writing HTML file for print the AST");
-            e.printStackTrace();
-        }
-    break;
-    case '\t':
-        takeIt();
-        writeSimpleBlack("&emsp;");
-    break;
-    default:
-      takeIt();
-      System.out.println("Error");
-      break;
+            }
+            break;
+        case ':':
+            takeIt();
+            if (currentChar == '=') {
+                takeIt();
+                writeSimpleBlack(this.currentSpelling.toString());
+            }else{
+                writeSimpleBlack(this.currentSpelling.toString());
+            }
+            break;
+        case ';':
+            takeIt();
+            writeSimpleBlack(this.currentSpelling.toString());
+            break;
+        case ',':
+            takeIt();
+            writeSimpleBlack(this.currentSpelling.toString());
+            break;
+        case '~':
+            takeIt();
+            writeSimpleBlack(this.currentSpelling.toString());
+            break;      
+        case '|':
+            takeIt();
+            writeSimpleBlack(this.currentSpelling.toString());
+            break;        
+        case '$':
+            takeIt();
+            writeSimpleBlack(this.currentSpelling.toString());
+            break;
+        case '(':
+            takeIt();
+            writeSimpleBlack(this.currentSpelling.toString());
+            break;
+        case ')':
+            takeIt();
+            writeSimpleBlack(this.currentSpelling.toString());
+            break;
+        case '[':
+            takeIt();
+            writeSimpleBlack(this.currentSpelling.toString());
+            break;
+        case ']':
+            takeIt();
+            writeSimpleBlack(this.currentSpelling.toString());
+            break;
+        case '{':
+            takeIt();
+            writeSimpleBlack(this.currentSpelling.toString());
+            break;
+        case '}':
+            takeIt();
+            writeSimpleBlack(this.currentSpelling.toString());
+            break;
+        case SourceFile.EOT:
+            this.isDone = true;
+            break;    
+        case '!':{
+            takeIt();
+            while ((currentChar != SourceFile.EOL) && (currentChar != SourceFile.EOT))
+                takeIt();
+            writeSimpleGreen(this.currentSpelling.toString());
+            break;
+        }  
+        case ' ':
+            takeIt();
+            writeSimpleBlack(this.currentSpelling.toString());
+        break;
+        case '\n':
+        case '\r': 
+            takeIt();
+            try {
+                fileWriter.write("<br>");
+            } catch (IOException e) {
+                System.err.println("Error while writing HTML file for print the AST");
+                e.printStackTrace();
+            }
+            break;
+        case '\t':
+            takeIt();
+            writeSimpleBlack("&emsp;");
+            break;
+        default:
+            takeIt();
+            System.out.println("Error");
+            break;
     }
-  }
-
-  public void scan () {
-    SourcePosition pos;
-
-    currentlyScanningToken = true;
-    currentSpelling = new StringBuffer("");
-    pos = new SourcePosition();
-    pos.start = sourceFile.getCurrentLine();
-    this.scanToken();
-    pos.finish = sourceFile.getCurrentLine();
   }
   
   public void generateHTML(){
       while(!this.isDone){
-          this.scan();
+          currentSpelling = new StringBuffer("");
+        this.scanToken();
       }
   }
   
@@ -266,34 +247,20 @@ public final class HTMLGenerator {
       return false;
   }
   private static String[] reservedWords = new String[] {
-    "array",
-    "choose",   // Se agrega el caracter de la palabra reservada choose
-    "const",
-    "do",
-    "else",
-    "elsif",    // Se agrega el caracter de la palabra reservada elsif
-    "end",      // Se agrega el caracter de la palabra reservada for
-    "for",      // Se agrega el caracter de la palabra reservada from
-    "from",
-    "func",
-    "if",
-    "in",
-    "let",
-    "loop",     // Se agrega el caracter de la palabra reservada loop
-    "nothing",  // Se agrega el caracter de la palabra reservada nothing
-    "of",
-    "package",  // Se agrega el caracter de la palabra reservada package
-    "private",  // Se agrega el caracter de la palabra reservada private
-    "proc",
-    "record",
-    "recursive",// Se agrega el caracter de la palabra reservada recursive
-    "then",
-    "to",       // Se agrega el caracter de la palabra reservada to
-    "type",
-    "until",    // Se agrega el caracter de la palabra reservada until
-    "var",
-    "when",     // Se agrega el caracter de la palabra reservada when
-    "while"
+    "array","choose",   // Se agrega el caracter de la palabra reservada choose
+    "const","do",
+    "else","elsif",     // Se agrega el caracter de la palabra reservada elsif
+    "end","for",        // Se agrega el caracter de la palabra reservada for
+    "from","func",      // Se agrega el caracter de la palabra reservada from
+    "if","in",
+    "let","loop",       // Se agrega el caracter de la palabra reservada loop
+    "nothing","of",
+    "package","private",// Se agrega el caracter de la palabra reservada private
+    "proc","record",
+    "recursive","then",
+    "to","type",
+    "until","var",
+    "when","while"
   };
   public void writeSimpleBlack(String word){
     try {
